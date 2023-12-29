d7d7e235729fdd0f62a108c6358f876081a99af1
kuba <radicals666@gmail.com>
2023-12-29 15:36:47 +0100

    balance js

--- static/js/bilans.js
+++ static/js/bilans.js
@@ -1,36 +1,39 @@
-console.log("opa")
 
-const counterDiv = document.querySelector(".counter")
-const checkboxes = document.querySelectorAll("#checkbox")
+const counterDiv = document.querySelector(".counter");
+const checkboxes = document.querySelectorAll("#checkbox");
+const tableBody = document.querySelector(".table-group-divider");
 
+let count = checkboxes.length;
+console.log(count);
 
+counterDiv.innerHTML += `<h1>${count}</h1>`;
 
-
-let count = checkboxes.length
-console.log(count)
-
-counterDiv.innerHTML+=`<h1>${count}</h1>`
-
-checkboxes.forEach((checkbox) => checkbox.addEventListener('change', function(){
-    if (checkbox.checked){
-        console.log("chekd")
-        count --
-    }else{
-        console.log("ozdznaczony")
-        count ++
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      count--;
+    } else {
+      count++;
     }
 
-    counterDiv.innerHTML = `<h1>${count}</h1>`
-}))
+    counterDiv.innerHTML = `<h1>${count}</h1>`;
 
+    // Sorting rows based on checkbox status
+    const rows = Array.from(tableBody.children);
+    rows.sort((a, b) => {
+      const checkboxA = a.querySelector("#checkbox");
+      const checkboxB = b.querySelector("#checkbox");
+      return checkboxA.checked - checkboxB.checked;
+    });
 
+    // Deleting existing rows from a table
+    tableBody.innerHTML = "";
 
-
-
-
-
-
-
-
+    // Adding sorted rows back to the table
+    rows.forEach((row) => {
+      tableBody.appendChild(row);
+    });
+  })
+);
 
 
