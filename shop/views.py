6d1168056d0101616bd311956d202b6ed8f35082
kuba <radicals666@gmail.com>
2024-02-28 13:55:10 +0100

    detail item template implement

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 
-from core.models import Item, Category, MainCategory
+from core.models import Item, Image, Category, MainCategory
+
+from django.contrib.sessions.models import Session
+from django.contrib.sessions.backends.db import SessionStore
 
 from .utils import get_paginate
+
+import json
 
 
 
@@ -16,7 +21,7 @@ def index_view(request):
      
      wedding_and_engagement = categories.filter(main_cat_name__main_name="Ślub i Zaręczyny")
 
-     print(old_jewelry)
+     # request.session.flush()
      context = {
          "categories": categories,
           "modern_jewelry": modern_jewelry,
@@ -67,10 +72,44 @@ def detail_shop_view(request, id):
      old_jewelry = categories.filter(main_cat_name__main_name="Biżuteria Dawna")
      wedding_and_engagement = categories.filter(main_cat_name__main_name="Ślub i Zaręczyny")
 
+     recently_viewed_items_data = []
+     others_items_in_category_data = []
 
 
      item = get_object_or_404(Item, id=id)
      images = item.image_set.all()
+
+     category = item.category.category_name
+     others_items_in_category = Item.objects.filter(category__category_name=category).exclude(id=item.id).order_by("?")[:5]
+     
+     for other_item in others_items_in_category:
+          other_images = other_item.image_set.all()
+          others_items_in_category_data.append({"other_item":other_item, "other_images": other_images})
+
+
+     if "recently_viewed" in request.session:
+          if item.inside_number in request.session["recently_viewed"]:
+               request.session["recently_viewed"].remove(item.inside_number)
+
+               recently_viewed_items = Item.objects.filter(inside_number__in=request.session["recently_viewed"])
+
+
+               for recently_item in recently_viewed_items:
+                    recently_viewed_images = recently_item.image_set.all()
+                    recently_viewed_items_data.append({"recently_item": recently_item, "images":recently_viewed_images})
+
+               
+
+          request.session["recently_viewed"].insert(0, item.inside_number)
+          if len(request.session["recently_viewed"]) > 5:
+               request.session["recently_viewed"].pop()
+     else:
+          request.session["recently_viewed"] = [item.inside_number]
+
+     
+     request.session.modified = True
+     
+     
 
      context = {
           "categories": categories,
@@ -78,7 +117,25 @@ def detail_shop_view(request, id):
           "old_jewelry": old_jewelry,
           "wedding_and_engagement": wedding_and_engagement,
           "item": item,
-          "images": images
+          "images": images,
+          "others_items_in_category_data": others_items_in_category_data,
+          # "recently_viewed_items": recently_viewed_items,
+          "recently_viewed_items_data": recently_viewed_items_data,
      }
 
      return render(request, "shop/detail_shop.html", context)
+
+
+# suggested_items = []
+#      viewed_items = request.session.get("viewed_items")
+
+#      if viewed_items is None:
+#           viewed_items = []
+
+#      item_inside_num = item.inside_number
+
+#      if item_inside_num not in viewed_items:
+#           viewed_items.append(item.inside_number)
+     
+#      request.session["viewed_items"] = viewed_items
+# # 
