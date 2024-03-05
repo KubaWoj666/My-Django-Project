70376b66dc8a57b7e95bf656d7876e469bee3e35
kuba <radicals666@gmail.com>
2024-03-05 14:53:26 +0100

    Corrections in the code and fixing a sessions related bug

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("categories/<slug:category_slug>/", views.category_view, name="categories"),
-    path("product-detail/<uuid:id>/", views.detail_shop_view, name="shop-detail"),
+    path("product-detail/<uuid:item_id>/", views.detail_shop_view, name="shop-detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,7 @@ from django.contrib.sessions.backends.db import SessionStore
 from .utils import get_paginate
 
 import json
-
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 def index_view(request):
@@ -66,7 +66,7 @@ def category_view(request, category_slug):
 
 
 
-def detail_shop_view(request, id):
+def detail_shop_view(request, item_id):
      categories = Category.objects.all()
      modern_jewelry = categories.filter(main_cat_name__main_name="Biżuteria Współczesna") 
      old_jewelry = categories.filter(main_cat_name__main_name="Biżuteria Dawna")
@@ -76,9 +76,10 @@ def detail_shop_view(request, id):
      others_items_in_category_data = []
 
 
-     item = get_object_or_404(Item, id=id)
+     item = get_object_or_404(Item, id=item_id)
      images = item.image_set.all()
 
+# Retrieving other items form same category
      category = item.category.category_name
      others_items_in_category = Item.objects.filter(category__category_name=category).exclude(id=item.id).order_by("?")[:5]
      
@@ -87,25 +88,23 @@ def detail_shop_view(request, id):
           others_items_in_category_data.append({"other_item":other_item, "other_images": other_images})
 
 
+# Retrieving if exist recently viewed items from the session.
      if "recently_viewed" in request.session:
           if item.inside_number in request.session["recently_viewed"]:
                request.session["recently_viewed"].remove(item.inside_number)
 
-               recently_viewed_items = Item.objects.filter(inside_number__in=request.session["recently_viewed"])
+          recently_viewed_items = Item.objects.filter(inside_number__in=request.session["recently_viewed"])
 
-
-               for recently_item in recently_viewed_items:
-                    recently_viewed_images = recently_item.image_set.all()
-                    recently_viewed_items_data.append({"recently_item": recently_item, "images":recently_viewed_images})
-
-               
+          for recently_item in recently_viewed_items:
+               recently_viewed_images = recently_item.image_set.all()
+               recently_viewed_items_data.append({"recently_item": recently_item, "images":recently_viewed_images})        
 
           request.session["recently_viewed"].insert(0, item.inside_number)
+          
           if len(request.session["recently_viewed"]) > 5:
                request.session["recently_viewed"].pop()
      else:
           request.session["recently_viewed"] = [item.inside_number]
-
      
      request.session.modified = True
      
@@ -119,23 +118,7 @@ def detail_shop_view(request, id):
           "item": item,
           "images": images,
           "others_items_in_category_data": others_items_in_category_data,
-          # "recently_viewed_items": recently_viewed_items,
           "recently_viewed_items_data": recently_viewed_items_data,
      }
 
      return render(request, "shop/detail_shop.html", context)
-
-
-# suggested_items = []
-#      viewed_items = request.session.get("viewed_items")
-
-#      if viewed_items is None:
-#           viewed_items = []
-
-#      item_inside_num = item.inside_number
-
-#      if item_inside_num not in viewed_items:
-#           viewed_items.append(item.inside_number)
-     
-#      request.session["viewed_items"] = viewed_items
-# # 
